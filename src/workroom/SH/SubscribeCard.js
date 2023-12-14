d7089024f9628bd6d5c3c133823ee5a0e538a3d7
YoHaiYo <12si47bun@naver.com>
2023-12-14 17:21:28 +0900

    구독섹션 컴포넌트화

--- src/workroom/SH/SubscribeCard.js
+++ src/workroom/SH/SubscribeCard.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function SubscribeCard() {
+
+function SubscribeCard(props) {
   return (
     <>
-    
+
+
+        <div class="subscribe-wrap">
+          <a href="">
+            {/* <img class='rounded-circle' src="/assets/img/cheongju/cheongju01.jpg" alt="cheongju01" /> */}
+            <img class='rounded-circle' src={props.img} alt="" />
+            <div class="box-name">{props.nm}</div>
+          </a>
+        </div>
+        
     </>
   )
 }
