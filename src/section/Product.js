1f601a34bc404dc9bcf757b6f358e3cd6352c23a
YoHaiYo <12si47bun@naver.com>
2023-12-15 19:17:19 +0900

    이중map으로 product 필터링구현. ul태그 내부에서만 되는거 수정필요

--- src/section/Product.js
+++ src/section/Product.js
@@ -1,15 +1,62 @@
-import React from 'react'
-import ProductCard from '../workroom/SH/ProductCard'
+import React, { useState } from 'react'
+// import ProductCard from '../workroom/SH/ProductCard'
 
 function Product(props) {
+  const [tabnum, setTabnum] = useState(0);
+
   return (
     
     <>
-
     <section id="itemTitle" class="mb80 mt80 wrap">
       <h2 class="text-center"><a href="" class="text-decoration-none">지금 당장 술 담아보기</a></h2>
+    <ul class="d-flex justify-content-around itemTitleBox">
+        {
+        props.datasrc.map((el,idx) => {
+        return (
+          <>
+          
+            <li key={`li${idx}`}  
+            class="d-flex flex-column align-items-center menu" 
+            data-filter=".set"
+            data-idx={idx}
+            onClick={() => {setTabnum(idx)}}
+            >
+              <img src={el.imgsrc} alt="담화박스"/>
+              <div class="itemTitle">{el.categorydata}</div>
+            </li>
 
-      <ul class="d-flex justify-content-around itemTitleBox">
+            {
+              tabnum === idx && (
+                el.productinfo.map((eel, iidx)=> {
+                  return (
+                    <div class="Imgbox " key={`div${iidx}`}> {/* 카테고리에 맞게 클래스명 */}          
+                        <a href="">
+                          <span><img src={eel.imgsrc} alt=""/></span>
+                          <div class="ItemDesc">
+                            <div class="productName ">{eel.productName}</div>
+                            <div class="productPrice ">{eel.productPrice}</div>
+                            <div class="starNum d-flex">{eel.starNum}</div>
+                            <p class="desc">{eel.desc}</p>
+                          </div>
+                        </a>
+                    </div>
+                  )
+                })
+              )                
+            }
+
+          </>
+        )
+      })            
+    }  
+    </ul> 
+    
+
+
+    <button class="sooldam-btn">더보기</button>
+    </section>
+
+      {/* <ul class="d-flex justify-content-around itemTitleBox">
         <li class="d-flex flex-column align-items-center menu" data-filter=".set">
           <img src="/assets/img/soolcatecoryicon/icon_cheongju.png" alt="담화박스"/>
           <div class="itemTitle">담화박스</div>
@@ -42,10 +89,9 @@ function Product(props) {
           <ProductCard datasrc={props.datasrc}/>
 
         </div>
-        <button>더보기</button>
-      </div>
+       
+      </div> */}
 
-    </section>
     </>
   )
 }
