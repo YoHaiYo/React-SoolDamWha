5fd10ca0f18cbde2352a9862cc1d26ebc54097ab
YoHaiYo <12si47bun@naver.com>
2023-12-15 15:07:43 +0900

    스와이퍼 컨텐츠 - 데이터로 빼기

--- src/section/Best.js
+++ src/section/Best.js
@@ -8,10 +8,7 @@ function Best() {
 
   return (
     <>
-       <section id="bestTitle" class="text-center mb-5">
-
-       <BestSwiper/>
-
+ <section id="bestTitle" class="text-center mb-5">
       <div class="d-flex align-items-center bg-transparent">
 
         <div class="slide-box position-relative">
@@ -26,79 +23,8 @@ function Best() {
           </div>
         </div>
 
-
-        {/* <div class="swiper">
-
-          <div class="swiper-wrapper">
-            <div class="swiper-slide">
-
-              <a href="">
-                <img src="/assets/img/cheongju/cheongju01.jpg" alt="" />
-              </a>
-              <div class="pickbox">
-                <div class="picktitle">구름을 벗삼아</div>
-                <div class="hashtag">#부드러운 #고급막걸리</div>
-              </div>
-              
-            </div>
-            <div class="swiper-slide">
-              <a href="">
-                <img src="/assets/img/cheongju/cheongju01.jpg" alt="" />
-              </a>
-              <div class="pickbox">
-                <div class="picktitle">나루 생막걸리</div>
-                <div class="hashtag">#부드러운 #생막걸리</div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <a href="">
-                <img src="/assets/img/cheongju/cheongju01.jpg" alt="" />
-              </a>
-              <div class="pickbox">
-                <div class="picktitle">담은 블랙</div>
-                <div class="hashtag">#부드러운 #고급막걸리</div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <a href="">
-                <img src="/assets/img/cheongju/cheongju01.jpg" alt="" />
-              </a>
-              <div class="pickbox">
-                <div class="picktitle">담은</div>
-                <div class="hashtag">#부드러운 #고급막걸리</div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <a href="">
-                <img src="/assets/img/cheongju/cheongju01.jpg" alt="" />
-              </a>
-              <div class="pickbox">
-                <div class="picktitle">대대포 블루</div>
-                <div class="hashtag">#부드러운 #고급막걸리</div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <a href="">
-                <img src="/assets/img/cheongju/cheongju01.jpg" alt=""/>
-              </a>
-              <div class="pickbox">
-                <div class="picktitle">막쿠</div>
-                <div class="hashtag">#부드러운 #고급막걸리</div>
-              </div>
-            </div>
-            <div class="swiper-slide">
-              <a href="">
-                <img src="/assets/img/cheongju/cheongju01.jpg" alt=""/>
-              </a>
-              <div class="pickbox">
-                <div class="picktitle">보라 막걸리</div>
-                <div class="hashtag">#부드러운 #고급막걸리</div>
-              </div>
-            </div>
-          </div>
-        </div> */}
-
-
+        <BestSwiper />
+       
       </div>
 
     </section>
--- src/workroom/SH/BestSwiper.js
+++ src/workroom/SH/BestSwiper.js
@@ -11,50 +11,43 @@ import 'swiper/css/navigation';
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
+import datasrc from '../../data/ssh.json'
+
 function BestSwiper() {
   return (
     <>
       <Swiper
         spaceBetween={10}
-        slidesPerView={1.5}
-        loop={false}
+        slidesPerView={6}
+        loop={true}
         centeredSlides={true}
         autoplay={{
           delay: 4000,
           disableOnInteraction: false,
+
         }}
     
         navigation={true}
-        modules={[Autoplay, Navigation]}
+        modules={[Autoplay, Navigation, Pagination]}
         className="BestSwiper"
       >
-        <SwiperSlide>
+            {
+      datasrc.bestswiper.map((el, idx)=> {
+        return (
+          <SwiperSlide key={`SwiperSlide${idx}`} >
           <a href="">
-            <img src="/assets/img/cheongju/cheongju01.jpg" alt="" />
+            <img src={el.imgsrc} alt="" />
           </a>
           <div class="pickbox">
-            <div class="picktitle">구름을 벗삼아</div>
-            <div class="hashtag">#부드러운 #고급막걸리</div>
+            <div class="picktitle">{el.picktitle}</div>
+            <div class="hashtag">{el.hashtag}</div>
           </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href="">
-            <img src="/assets/img/cheongju/cheongju01.jpg" alt="" />
-          </a>
-          <div class="pickbox">
-            <div class="picktitle">구름을 벗삼아</div>
-            <div class="hashtag">#부드러운 #고급막걸리</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href="">
-            <img src="/assets/img/cheongju/cheongju01.jpg" alt="" />
-          </a>
-          <div class="pickbox">
-            <div class="picktitle">구름을 벗삼아</div>
-            <div class="hashtag">#부드러운 #고급막걸리</div>
-          </div>
-        </SwiperSlide>
+        </SwiperSlide>     
+          )
+      })
+      
+    } 
+ 
       </Swiper>  
     </>
   )
