2217b89f8496d1927968867d3e104833a738ccd2
jiyy25 <jiyy825@gmail.com>
2023-12-19 15:15:49 +0900

    itemBox태그 수정

--- src/workroom/JY/Item.js
+++ src/workroom/JY/Item.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Itembox from './Itembox';
 import sdhdata from '../../data/sdhdata.json'
 import Category from './Category';
 import Itembox from './Itembox';
@@ -10,7 +9,7 @@ function Item() {
         <section id="itemTitle" className="mb80 mt80 wrap container">
             <h2 className="text-center"><a href="#closeTop" className="text-decoration-none scrollTop">지금 당장 술 담아보기</a></h2>
             <Category sdhdt={sdhdata.sdh_category}></Category>
-            <Itembox sdhdata={sdhdata}></Itembox>
+            <Itembox sdhdt={sdhdata}></Itembox>
         </section>
 
 
--- src/workroom/JY/Itembox.js
+++ src/workroom/JY/Itembox.js
@@ -5,20 +5,22 @@ function Itembox() {
         <div className="itemImgWrap ">
             <div className="itemImgWrap_margin d-flex flex-wrap ">
                 <div className="Imgbox gwashilju">
-                    <a href="">
-                        <span><img src="/assets/img/set/set01.jpg" alt="" /></span>
-                        <div className="ItemDesc">
-                            <div className="productName ">샤인머스켓와인</div>
-                            <div className="productPrice ">12000원</div>
-                            <div className="starNum d-flex">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
-                            <p className="desc">달콤한 샤인머스켓 향이 듬뿍~</p>
+                    <div>
+                        <a href="">
+                            <img src="/assets/img/set/set01.jpg" alt="" />
+                        </a>
+                    </div>
+                    <div className="ItemDesc">
+                        <div className="productName "><a href="">샤인머스켓와인</a></div>
+                        <div className="productPrice ">12000원</div>
+                        <div className="starNum d-flex">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
                         </div>
-                    </a>
+                        <p className="desc">달콤한 샤인머스켓 향이 듬뿍~</p>
+                    </div>
                 </div>
             </div>
         </div>
