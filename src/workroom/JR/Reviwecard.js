4b9835392cc2846c03d7c7b8ae09b06049a0750e
kimjaer <y5g5776@naver.com>
2023-12-19 15:50:14 +0900

    스와이퍼 재생됨

--- src/workroom/JR/Reviwecard.js
+++ src/workroom/JR/Reviwecard.js
@@ -14,12 +14,14 @@ export default function ReviewCard() {
   return (
     <>
       <Swiper
-        spaceBetween={30}
         centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
+        // spaceBetween={8}
+        // slidesPerView={7}
+        // speed={6000}
+        // autoplay={{
+        //   delay: 0,
+        //   disableOnInteraction: false,
+        // }}
         pagination={{
           clickable: true,
         }}
@@ -27,26 +29,32 @@ export default function ReviewCard() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <div id='review'>
-            {/* <img src="/assets/img/takju/takju1.jpg" alt="" className="ptrv"/> */}
-              <p className='color-black'>어울리는 안주 만들어 남편과 같이 마셔봐야겠어요.</p>
-              <div id="face" class="d-flex justify-content-center">
-              <a className="face"><img src='/assets/img/face/face1-01.svg' alt=""/>re15님</a>
-                <a>
-                  <div className="rv-star">
-                    <div className="starNum d-flex text-center w-100">
-                      <span></span>
-                      <span></span>
-                      <span></span>
-                      <span></span>
-                      <span></span>
+        {
+          [1, 2, 3, 4].map((e, i) => {
+            return (
+              <SwiperSlide>
+                <div id='review'>
+                  <img src="/assets/img/takju/takju1.jpg" alt="" className="ptrv" />
+                  <p className='color-black'>어울리는 안주 만들어 남편과 같이 마셔봐야겠어요.</p>
+                  <div id="face" className="d-flex justify-content-center">
+                    <a className='face'><img src='/assets/img/face/face1-01.svg' alt="" />re15님</a>
+                    <div className="rv-star">
+                      <div className="starNum d-flex text-center w-100">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                      </div>
                     </div>
                   </div>
-                </a>
-              </div>
-          </div>
-        </SwiperSlide>
+                </div>
+              </SwiperSlide>
+            )
+
+          })
+
+        }
       </Swiper>
     </>
   );
