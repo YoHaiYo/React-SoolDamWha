753c90f16cdbdaa533de7f671b91d5f519d77424
YoHaiYo <12si47bun@naver.com>
2024-01-10 16:48:40 +0900

    구독섹션 모바일시 탭메뉴로 변경

--- src/section/Best.js
+++ src/section/Best.js
@@ -8,25 +8,29 @@ function Best(props) {
 
   return (
     <>
- <section id="bestTitle" className="text-center mb-5">
-      <div className="d-flex align-items-center bg-transparent">
+      <section id="bestTitle" className="text-center mb-5">
 
-        <div className="slide-box">
-          <h2 className="best-ment">
-            {props.datasrc.etc.title1}<br/>
-            {props.datasrc.etc.title2}
-          </h2>
-          <button className="ssh--btn-go-now">
-          {props.datasrc.etc.btn}
-            <i className="bi bi-box-arrow-up-right"></i>
-          </button>
+        <h2 className="best-ment mo-title">
+          {props.datasrc.etc.title1} {props.datasrc.etc.title2}
+        </h2>
+        <div className="d-flex align-items-center bg-transparent">
+
+          <div className="slide-box">
+            <h2 className="best-ment">
+              {props.datasrc.etc.title1}<br />
+              {props.datasrc.etc.title2}
+            </h2>
+            <button className="ssh--btn-go-now">
+              {props.datasrc.etc.btn}
+              <i className="bi bi-box-arrow-up-right"></i>
+            </button>
+          </div>
+
+          <BestSwiper datasrc={props.datasrc.item} />
+
         </div>
 
-        <BestSwiper datasrc={props.datasrc.item}/>
-       
-      </div>
-
-    </section>
+      </section>
     </>
   )
 }
