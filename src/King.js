b858b2e43f9fd0105f7e2cb7fa804ad20f2717bd
YoHaiYo <12si47bun@naver.com>
2024-01-10 17:57:39 +0900

    라우터처리 전체상품 페이지 추가중

--- src/King.js
+++ src/King.js
@@ -1,5 +1,8 @@
-
 import React, { useState } from 'react';
+import { Route, Routes } from 'react-router';
+
+// data
+import datasrc from './data/sdhdata.json'
 
 // Section
 import Footer from './section/Footer';
@@ -10,9 +13,8 @@ import Product from './section/Product';
 import Review from './section/Review';
 import Contact from './section/Contact';
 
-// data
-// import sdhdata from './data/sdhdata.json'
-import datasrc from './data/sdhdata.json'
+// Page
+import AllPage from './page/AllPage';
 
 import LanguageSwitcher from './LanguageSwitcher'; // 새로 추가한 부분
 
@@ -21,23 +23,34 @@ const King = () => {
 
   // changeLanguage : useState로 작성된 setLanguage로 언어선택 텍스트를 바꿔주는 함수
   // language : 각 컴포넌트에 뿌리는 datasrc의 객체접근을 ko,en 으로 변경해주는 변수
-  const changeLanguage = (newLanguage) => { 
+  const changeLanguage = (newLanguage) => {
     setLanguage(newLanguage);
   };
   return (
     <>
+      {/* 네비 */}
       <LanguageSwitcher changeLanguage={changeLanguage} /> {/* 언어 변경 컴포넌트 추가 */}
       <Header datasrc={datasrc[language].header} />
-      <Subscribe datasrc={datasrc[language].subscribe} />
-      <Best datasrc={datasrc[language].bestswiper} />
-      <Product datasrc={datasrc[language].product}></Product>
-      <Review />
-      <Contact />
-      <Footer />
+      <Routes>
+        {/* 초기페이지 */}
+        <Route path='/'
+          element={<>
+            <Subscribe datasrc={datasrc[language].subscribe} />
+            <Best datasrc={datasrc[language].bestswiper} />
+            <Product datasrc={datasrc[language].product}></Product>
+            <Review />
+            <Contact />
+            <Footer />
+          </>}>
+        </Route>
+
+        <Route path='/all' element={<AllPage datasrc={datasrc[language].product} />}></Route>
+      </Routes>
+
+
+
     </>
   );
 };
 
 export default King;
-
-
