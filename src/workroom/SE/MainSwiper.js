5aa7d08a2168e3700567701f34078f187898eaa7
YoHaiYo <12si47bun@naver.com>
2024-01-15 21:05:30 +0900

    deploy용 브런치생성(경로꼬임 방지용)

--- src/workroom/SE/MainSwiper.js
+++ src/workroom/SE/MainSwiper.js
@@ -3,12 +3,13 @@ import { EffectFade, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 // import mainS from '../../data/MainSwiper.json'
+import React from 'react';
 
 
 export default (props) => {
     return (
         <div>
-            <Swiper id='promotionSwiper'
+            <Swiper id='promotionSwiper' style={{ marginTop: "0px" }}
                 modules={[EffectFade, Autoplay]} effect="fade"
                 loop={true}
                 autoplay={{
@@ -18,19 +19,34 @@ export default (props) => {
 
                 spaceBetween={50}
                 slidesPerView={1}
-                // onSlideChange={() => console.log('slide change')}
-                // onSwiper={(swiper) => console.log(swiper)}
+            // onSlideChange={() => console.log('slide change')}
+            // onSwiper={(swiper) => console.log(swiper)}
 
             >
                 {
                     props.datasrc.map((el, idx) => {
                         return <SwiperSlide style={{
-                            background: `url(/assets/img/swiper_img/${el.background}) no-repeat center`, height: "480px", backgroundSize: "auto 100%"
+                            background: `url(./assets/img/swiper_img/${el.background}) no-repeat center`, height: "480px", backgroundSize: "auto 100%"
                         }} key={idx}>
                             <div className='effecttext'>
 
-                                <strong>{el.strong}</strong>
-                                <p>{el.Mtext}</p>
+                                <strong>{
+                                    el.strong.split("|").map((e, i) => {
+                                        return <React.Fragment>
+                                            {e}
+                                            <br />
+                                        </React.Fragment>
+
+                                    })
+                                }</strong>
+                                <p>{el.Mtext.split("|").map((e, i) => {
+                                    return (
+                                        <React.Fragment key={i}>
+                                            {e}
+                                            <br />
+                                        </React.Fragment>
+                                    )
+                                })}</p>
                             </div >
 
                         </SwiperSlide >
