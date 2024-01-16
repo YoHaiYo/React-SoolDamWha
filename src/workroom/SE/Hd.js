244d5841d6609f341cd9c8652ad2ee432da30ad7
YoHaiYo <12si47bun@naver.com>
2024-01-16 11:24:04 +0900

    라우터 오류 이슈해결

--- src/workroom/SE/Hd.js
+++ src/workroom/SE/Hd.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 // import Resist from './resist'
 // import gnbdb from '../../data/gnb.json'
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,31 +12,32 @@ function HD(props) {
     const scrolldown = () => {
         document.querySelector("#hd").classList = window.scrollY > 0 ? "fixed-top bg-white border-bottom down" : "fixed-top bg-white border-bottom";
         document.body.classList = window.scrollY > 0 ? " down" : "";
-
-
     }
     useEffect(() => {
         window.addEventListener("scroll", scrolldown)
         return () => {
             window.removeEventListener("scroll", scrolldown)
         }
-
     }, [])
 
     return (
         <header id="hd" className='fixed-top bg-white border-bottom'>
             <div className='text-center container-lg'>
                 <h1 id='Logo'>
-                    <a href="/">
+                    <Link to={`/`}>
                         <img src="/assets/img/icon/hd_logo.png" alt="로고" />
                         <img src="/assets/img/icon/sub_logo.png" alt="로고" />
-                    </a>
+                    </Link>
                 </h1>
                 <ul id="gnb" className='d-flex justify-content-center'>
                     {
                         props.datasrc.map((v, i) => {
                             return (
-                                <li key={i}><a href={v.href}>{v.gnbtext}</a></li>
+
+                                <li key={i}>
+                                    {/* <a href={v.href}>{v.gnbtext}</a>  */}
+                                    <Link to={`/${v.href}`}>{v.gnbtext}</Link>
+                                </li>
                             )
                         })
                     }
