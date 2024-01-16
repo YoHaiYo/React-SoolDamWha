38061a964c5e5ec6728c45e7913fc5fd81d88458
YoHaiYo <12si47bun@naver.com>
2024-01-16 12:45:08 +0900

    메인배너 이벤트 링크로 연결

--- src/workroom/SE/MainSwiper.js
+++ src/workroom/SE/MainSwiper.js
@@ -2,57 +2,61 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
+import { Link } from 'react-router-dom';
+
 // import mainS from '../../data/MainSwiper.json'
 import React from 'react';
 
 
 export default (props) => {
     return (
-        <div>
-            <Swiper id='promotionSwiper' style={{ marginTop: "280px" }}
-                modules={[EffectFade, Autoplay]} effect="fade"
-                loop={true}
-                autoplay={{
-                    delay: 2500,
+        <Link to={`/event`}>
+            <div>
+                <Swiper id='promotionSwiper' style={{ marginTop: "280px" }}
+                    modules={[EffectFade, Autoplay]} effect="fade"
+                    loop={true}
+                    autoplay={{
+                        delay: 2500,
 
-                }}
+                    }}
 
-                spaceBetween={50}
-                slidesPerView={1}
-            // onSlideChange={() => console.log('slide change')}
-            // onSwiper={(swiper) => console.log(swiper)}
+                    spaceBetween={50}
+                    slidesPerView={1}
+                // onSlideChange={() => console.log('slide change')}
+                // onSwiper={(swiper) => console.log(swiper)}
 
-            >
-                {
-                    props.datasrc.map((el, idx) => {
-                        return <SwiperSlide style={{
-                            background: `url(./assets/img/swiper_img/${el.background}) no-repeat center`, height: "480px", backgroundSize: "auto 100%"
-                        }} key={idx}>
-                            <div className='effecttext' key={idx}>
+                >
+                    {
+                        props.datasrc.map((el, idx) => {
+                            return <SwiperSlide style={{
+                                background: `url(./assets/img/swiper_img/${el.background}) no-repeat center`, height: "480px", backgroundSize: "auto 100%"
+                            }} key={idx}>
+                                <div className='effecttext' key={idx}>
 
-                                <strong>{
-                                    el.strong.split("|").map((e, i) => {
-                                        return <React.Fragment key={i}>
-                                            {e}
-                                            <br />
-                                        </React.Fragment>
+                                    <strong>{
+                                        el.strong.split("|").map((e, i) => {
+                                            return <React.Fragment key={i}>
+                                                {e}
+                                                <br />
+                                            </React.Fragment>
 
-                                    })
-                                }</strong>
-                                <p>{el.Mtext.split("|").map((e, i) => {
-                                    return (
-                                        <React.Fragment key={i}>
-                                            {e}
-                                            <br />
-                                        </React.Fragment>
-                                    )
-                                })}</p>
-                            </div >
+                                        })
+                                    }</strong>
+                                    <p>{el.Mtext.split("|").map((e, i) => {
+                                        return (
+                                            <React.Fragment key={i}>
+                                                {e}
+                                                <br />
+                                            </React.Fragment>
+                                        )
+                                    })}</p>
+                                </div >
 
-                        </SwiperSlide >
-                    })
-                }
-            </Swiper >
-        </div >
+                            </SwiperSlide >
+                        })
+                    }
+                </Swiper >
+            </div >
+        </Link>
     );
 };
