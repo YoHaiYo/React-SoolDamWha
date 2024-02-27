c08e413dfc2d63d0797b018213f7ae6fd6a782a3
kimjaer <wofus0422@gmail.com>
2024-02-27 16:47:51 +0900

    qna 페이지 자주묻는 질물 width 수정, 입점문의 form 생성 완료

--- src/page/Qna.js
+++ src/page/Qna.js
@@ -9,7 +9,7 @@ const Qna = (props) => {
 
 
   return (
-    <section ref={faq} id="faq" className="align-items-center" style={{ marginTop: "280px", marginBottom: "100px" }}>
+    <section ref={faq} id="faq" className="align-items-center container" style={{ marginTop: "280px", marginBottom: "100px", width:"50%" }}>
       <h2>Q. 자주묻는 질문</h2><br /><br />
       <div className="d-flex justify-content-center align-items-center">
         <ul>
@@ -42,6 +42,43 @@ const Qna = (props) => {
           }
         </ul>
       </div>
+
+
+        <div id="qna">
+          <div className="qnabox">
+            <form action="#" name="contact-info" method="post" className="container">
+              
+              <div className="text-center">
+              <h2>Q.입점문의</h2>
+                    <div className='nmemailzone'>
+                      <div className="nmzone p-1">
+                        <label for="name">성함</label>
+                        <input className="" type="text" name="name" placeholder="성함" required />
+                      </div>
+                      
+                      <div className="emailzsone p-1">
+                          <label for="email">이메일</label>
+                          <input type="email" name="email" placeholder="example@example.com" required />
+                      </div>
+                    </div>
+
+
+                    <div className="msgzone">
+                      <textarea className="message" name="message" cols="51" rows="5" placeholder="문의사항을 입력해 주시면 이메일로 답변해드려요^^" required></textarea>
+                    </div>
+              </div>
+
+                  <div className="send">
+                    <div>
+                      <input type="submit" name="send-btn" value="보내기" onclick='sendInfo()' className="sooldam-btn mt-3" />
+                    </div>
+                  </div>
+
+            </form>
+          </div>
+        </div>
+
+
     </section>
   )
 }
