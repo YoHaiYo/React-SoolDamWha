ceccc3dadfe5e8096fae83ab623b43f02f101205
SeungEunChun <1000tmddms@gmail.com>
2024-03-12 14:41:07 +0900

    새로고침하면 해당 라우팅을 다시 찾아갈수 있도록 모든 페이지에게 index.html 반환

--- project/src/index.js
+++ project/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import King from './King.js';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import ScrollToTop from './workroom/SE/ScrollTopTop.js';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
+    <ScrollToTop />
     <King />
   </BrowserRouter>
   // </React.StrictMode>
---
+++ project/src/workroom/SE/ScrollTopTop.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'instant' //behavior 안의value값으로 instant가 들어감으로써 에니메이션 없이 바로 최상단으로 이동
+        });
+    }, [pathname]);
+
+    return null;
+}
